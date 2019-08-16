8dd78c78c1b622222f9b2c004ce402e35066d1e8
hurak <hurak@fel.cvut.cz>
2019-08-16 15:49:14 +0200

    The whole conversions.jl file upgraded to Julia >= 1.0

--- src/conversions.jl
+++ src/conversions.jl
@@ -1,10 +1,10 @@
 # PolyMatrix and PolyMatrix
-promote_rule{T1,T2,M1,M2,W,N}(::Type{PolyMatrix{T1,M1,Val{W},N}},
-  ::Type{PolyMatrix{T2,M2,Val{W},N}}) =
+promote_rule(::Type{PolyMatrix{T1,M1,Val{W},N}},
+  ::Type{PolyMatrix{T2,M2,Val{W},N}}) where {T1,T2,M1,M2,W,N} =
   PolyMatrix{promote_type(T1, T2), promote_type(M1,M2), Val{W}, N}
 
-function _convert{T1,N,T2,M1,M2,W}(::Type{PolyMatrix{T1,M1,Val{W},N}},
-  p::PolyMatrix{T2,M2,Val{W},N})
+function _convert(::Type{PolyMatrix{T1,M1,Val{W},N}},
+  p::PolyMatrix{T2,M2,Val{W},N}) where {T1,N,T2,M1,M2,W}
   cr = SortedDict(Dict{Int,M1}())
   for (k,c) in coeffs(p)
     insert!(cr, k, map(x->convert(T1,x),c))
@@ -12,8 +12,8 @@ function _convert{T1,N,T2,M1,M2,W}(::Type{PolyMatrix{T1,M1,Val{W},N}},
   PolyMatrix(cr, size(p), Val{W})
 end
 
-@generated function convert{T1,N,T2,M1,M2,W}(::Type{PolyMatrix{T1,M1,Val{W},N}},
-  p::PolyMatrix{T2,M2,Val{W},N})
+@generated function convert(::Type{PolyMatrix{T1,M1,Val{W},N}},
+  p::PolyMatrix{T2,M2,Val{W},N}) where {T1,N,T2,M1,M2,W}
   if !(M1 <: AbstractArray{T1,N})
     return :(error("convert: first two parameters of first argument are incompatible"))
   elseif T1 == T2 && M1 == M2
@@ -24,8 +24,8 @@ end
 end
 
 # PolyMatrix and AbstractArray
-#promote_rule{T1,T2,M1,W,N}(::Type{PolyMatrix{T1,M1,Val{W},N}},
-#  ::Type{AbstractArray{T2,N}}) = PolyMatrix{promote_type(T1, T2), M1, Val{W}, N}
+#promote_rule(::Type{PolyMatrix{T1,M1,Val{W},N}},
+#  ::Type{AbstractArray{T2,N}}) where {T1,T2,M1,W,N} = PolyMatrix{promote_type(T1, T2), M1, Val{W}, N}
 
-#convert{T1,T2,M1,W,N}(::Type{PolyMatrix{T1,M1,Val{W},N}},
-#  p::AbstractArray{T2,N}) = PolyMatrix(p, size(p), Val{W})
+#convert(::Type{PolyMatrix{T1,M1,Val{W},N}},
+#  p::AbstractArray{T2,N}) where {T1,T2,M1,W,N} = PolyMatrix(p, size(p), Val{W})
